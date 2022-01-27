7129c92dfe4afbfa286e1d56c3059801ccb748ae
Erik-EFL <erik.lima.f@outlook.com>
2022-01-27 18:13:51 -0300

    corrigindo requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,12 +3,11 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   // pair programming com Sheila Nakashima
   const caringPerson = data.employees
-    .find((employee) => employee.id === id)
+    .filter((employee) => employee.id === id)
     .map((animal) => animal.responsibleFor[0]);
-  const findOlder = data.species
-    .find((name) => name.id === caringPerson[0])
-    .residents.sort((one, two) => one.age - two.age)
-    .map((resident) => [resident.name, resident.sex, resident.age]);
+  const findOlder = data.species.find((name) => name.id === caringPerson[0])
+    .residents.sort((one, two) => two.age - one.age)
+    .map((resident) => [resident.name, resident.sex, resident.age])[0];
 
   return findOlder;
 }
