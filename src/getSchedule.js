a1e1394e61864ec538744bba23578a87bd3b70df
Erik-EFL <erik.lima.f@outlook.com>
2022-01-28 02:16:02 -0300

    adicionando requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,46 @@
 const { hours, species } = require('../data/zoo_data');
 
-const week = Object.keys(hours);
-const arca = species.reduce((acc, animal) => {
-  acc.push(animal.name);
-  return acc;
+const arca = species.reduce((accumulator, animal) => {
+  accumulator.push(animal.name);
+  return accumulator;
 }, []);
 
-console.log(arca);
-console.log(week);
-/* function getSchedule(scheduleTarget) {
+const getAnimalDay = (day) => {
+  const filterAnimal = species.filter((present) => present.availability.includes(day));
+  return filterAnimal.reduce((accumulator, animal) => {
+    accumulator.push(animal.name);
+    return accumulator;
+  }, []);
+};
 
+const operation = () => Object.keys(hours).reduce((accumulator, day) => {
+  if (day === 'Monday') {
+    accumulator[day] = {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+  } else {
+    accumulator[day] = {
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+      exhibition: getAnimalDay(day),
+    };
+  }
+  return accumulator;
+}, {});
+
+function getSchedule(scheduleTarget) {
+  if (Object.keys(hours).includes(scheduleTarget)) {
+    const listObject = {};
+    listObject[scheduleTarget] = operation()[scheduleTarget];
+    return listObject;
+  }
+  if (arca.includes(scheduleTarget)) {
+    return species.find((animal) => animal.name === scheduleTarget).availability;
+  }
+  return operation();
 }
 
-module.exports = getSchedule; */
+module.exports = getSchedule;
+
+// console.log(arca.length);
+// console.log(aval);
