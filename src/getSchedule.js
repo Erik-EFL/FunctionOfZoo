3b90e64fde20d6ea4f04a0887e3788bf2d34cbed
Erik-EFL <erik.lima.f@outlook.com>
2022-01-30 16:10:14 -0300

    Fazendo correções no requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -6,7 +6,9 @@ const arca = species.reduce((accumulator, animal) => {
 }, []);
 
 const getAnimalDay = (day) => {
+  // Verifica e filtra se nos dias de aprestação dos animais tem também os dias da semana
   const filterAnimal = species.filter((present) => present.availability.includes(day));
+  // Monta um array com os dias de apresentação e os animais que estão presentes naquele dia
   return filterAnimal.reduce((accumulator, animal) => {
     accumulator.push(animal.name);
     return accumulator;
@@ -15,10 +17,7 @@ const getAnimalDay = (day) => {
 
 const operation = () => Object.keys(hours).reduce((accumulator, day) => {
   if (day === 'Monday') {
-    accumulator[day] = {
-      officeHour: 'CLOSED',
-      exhibition: 'The zoo will be closed!',
-    };
+    accumulator[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
   } else {
     accumulator[day] = {
       officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
@@ -28,19 +27,18 @@ const operation = () => Object.keys(hours).reduce((accumulator, day) => {
   return accumulator;
 }, {});
 
-function getSchedule(scheduleTarget) {
+const getSchedule = (scheduleTarget) => {
+  // Retorna todos os dias de apresentação caso o paramento esteja vazio
   if (Object.keys(hours).includes(scheduleTarget)) {
     const listObject = {};
     listObject[scheduleTarget] = operation()[scheduleTarget];
     return listObject;
   }
+  // Retorna o animal especifico e seus dias de apresentação.
   if (arca.includes(scheduleTarget)) {
     return species.find((animal) => animal.name === scheduleTarget).availability;
   }
   return operation();
-}
+};
 
 module.exports = getSchedule;
-
-// console.log(arca.length);
-// console.log(aval);
